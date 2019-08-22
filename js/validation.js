377a61be8cc3341f02745b01f0b9209fb60ddb6b
Droideka <vindou@mail.ru>
2019-08-22 03:49:26 +0300

    Validation form

--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,4 @@
-let form = document.form1
+let form = document.form1;
 form.addEventListener('submit', (e) => {
    let name = form.name,
        phone = form.phone;
@@ -6,4 +6,14 @@ form.addEventListener('submit', (e) => {
       alert('Все поля должны быть заполненны!');
       e.preventDefault();
    }  
-})
+});
+let name = form.name;
+name.addEventListener('input', (e) => {
+   if (name.value.length < 3) name.style.borderBottom = '2px solid red';
+   else name.style.borderBottom = '2px solid green'
+});
+let phone = form.phone;
+phone.addEventListener('change', (e) => {
+   if (/^\+?38[0-9]{8,14}$/.test(phone.value)) phone.style.borderBottom = '2px solid green';
+   else phone.style.borderBottom = '2px solid red';
+});
